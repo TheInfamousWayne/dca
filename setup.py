3b0ebb69400402a3904da2d96645d16e6b2e2be3
theinfamouswayne <vaibhav.agrawal@tuebingen.mpg.de>
2019-12-05 00:59:59 +0100

    auto installation

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
                       'scikit-learn',
                       'scanpy',
                       'kopt',
+                      'tensorflow>=2.0'
                       'pandas' #for preprocessing
                       ],
     url='https://github.com/theinfamouswayne/dca',
